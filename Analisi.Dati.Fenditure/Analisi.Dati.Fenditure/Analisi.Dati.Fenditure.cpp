736f3476246107022109cc4871b0df38623c9336
Campostrini <Campostrini@users.noreply.github.com>
2016-04-04 19:35:31 +0200

    Inizializzazione
    Base dell'analisi

---
+++ Analisi.Dati.Fenditure/Analisi.Dati.Fenditure/Analisi.Dati.Fenditure.cpp
@@ -0,0 +1,97 @@
+// Analisi.Dati.Fenditure.cpp : definisce il punto di ingresso dell'applicazione console.
+//
+
+#include "stdafx.h"
+#include "BasicExcel.hpp"
+#include "ExcelFormat.h"
+#include "math.h"
+
+#ifdef _WIN32
+
+#define WIN32_LEAN_AND_MEAN
+
+
+#include <windows.h>
+#include <shellapi.h>
+#include <crtdbg.h>
+
+#else // _WIN32
+
+
+#define	FW_NORMAL	400
+#define	FW_BOLD		700
+
+#endif // _WIN32
+double sind(double gradi);
+
+double cosd(double gradi);
+
+double convertiSecondiInGradi(double secondi);
+
+double modulo(double val);
+
+double gradiRadianti(double gradi);
+
+double square(double value);
+
+
+double convertiSecondiInGradi(double secondi) {
+
+	int Gradi = secondi;
+
+	double Gr = Gradi;
+
+	double finale = ((secondi - Gr) / 0.6) + Gr;
+
+	return finale;
+
+};
+
+double gradiRadianti(double gradi) {
+	const double M_PI = 4 * atan(1);
+	return gradi*M_PI / 180;
+};
+
+double sind(double gradi) {
+	const double M_PI = 4 * atan(1);
+	return sin((gradi)* M_PI / 180);
+};
+
+double cosd(double gradi) {
+	const double M_PI = 4 * atan(1);
+	return cos((gradi)* M_PI / 180);
+};
+
+double modulo(double val) {
+	if (val < 0.0) {
+		return -val;
+	}
+	else {
+		return val;
+	};
+};
+
+double square(double value) {
+	return value*value;
+};
+
+
+
+
+
+
+
+int main()
+{
+	YExcel::BasicExcel f1("1 fenditura.xls");
+	YExcel::BasicExcel f2("2 fenditure.xls");
+	YExcel::BasicExcel f3("3 fenditure.xls");
+	YExcel::BasicExcel f4("4 fenditure.xls");
+	YExcel::BasicExcelWorksheet* f1Sheet = f1.GetWorksheet(0);
+	YExcel::BasicExcelWorksheet* f2Sheet = f2.GetWorksheet(0);
+	YExcel::BasicExcelWorksheet* f3Sheet = f3.GetWorksheet(0);
+	YExcel::BasicExcelWorksheet* f4Sheet = f4.GetWorksheet(0);
+
+    return 0;
+}
+

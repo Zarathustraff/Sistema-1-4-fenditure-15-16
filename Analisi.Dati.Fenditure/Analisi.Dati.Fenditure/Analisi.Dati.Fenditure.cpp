31cb6e0b7a49677d3a0a201887ffb68d7316ca1e
Campostrini <Campostrini@users.noreply.github.com>
2016-04-08 14:37:59 +0200

    Inizializzazione Dati Corretta. Prossimo step: P1D
    Manca l'analisi dei massimi e dei minimi

--- Analisi.Dati.Fenditure/Analisi.Dati.Fenditure/Analisi.Dati.Fenditure.cpp
+++ Analisi.Dati.Fenditure/Analisi.Dati.Fenditure/Analisi.Dati.Fenditure.cpp
@@ -85,19 +85,73 @@ double square(double value) {
 
 int main()
 {
-	YExcel::BasicExcel f1("1 fenditura.xls");
-	YExcel::BasicExcel f2("2 fenditure.xls");
-	YExcel::BasicExcel f3("3 fenditure.xls");
-	YExcel::BasicExcel f4("4 fenditure.xls");
-	YExcel::BasicExcelWorksheet* f1Sheet = f1.GetWorksheet(0);
-	YExcel::BasicExcelWorksheet* f2Sheet = f2.GetWorksheet(0);
-	YExcel::BasicExcelWorksheet* f3Sheet = f3.GetWorksheet(0);
-	YExcel::BasicExcelWorksheet* f4Sheet = f4.GetWorksheet(0);
+	for (int a = 0; a == 0; a++) {
+		try {
+			YExcel::BasicExcel f1("1.xls");
+			if (!(f1.file_.IsOpen())) {
+				throw 1;
+				break;
+			};
+			
+			YExcel::BasicExcel f2("2.xls");
+			if (!(f2.file_.IsOpen())) {
+				throw 2;
+				break;
+			};
+			YExcel::BasicExcel f3("3.xls");
+			if (!(f3.file_.IsOpen())) {
+				throw 3;
+				break;
+			};
+			YExcel::BasicExcel f4("4.xls");
+			if (!(f4.file_.IsOpen())) {
+				throw 4;
+				break;
+			};
 
-	vector<long> f1L, f2L, f3L, f4L;
-	vector<float> f1F, f2F, f3F, f4F;
+			YExcel::BasicExcelWorksheet* f1Sheet = f1.GetWorksheet(0);
+			YExcel::BasicExcelWorksheet* f2Sheet = f2.GetWorksheet(0);
+			YExcel::BasicExcelWorksheet* f3Sheet = f3.GetWorksheet(0);
+			YExcel::BasicExcelWorksheet* f4Sheet = f4.GetWorksheet(0);
 
-	for (int row = 0; f1Sheet->Cell(row,1)->)
+			
+			vector<float> f1F, f2F, f3F, f4F;
+			vector<double> f1D, f2D, f3D, f4D;
+
+			for (int row = 0; row<400; row++) {
+				
+				f1D.push_back(f1Sheet->Cell(row, 1)->GetDouble());
+				f1F.push_back(f1D[row] /*/ 10000*/);
+				std::cout <<row<<": "<< f1Sheet->Cell(row, 1)->GetDouble() << ": " << f1D[row] << ": " << f1F[row] << endl;
+			};
+			for (int row = 0; row<400; row++) {
+
+				f2D.push_back(f2Sheet->Cell(row, 1)->GetDouble());
+				f2F.push_back(f2D[row] /*/ 10000*/);
+				std::cout << row << ": " << f1Sheet->Cell(row, 1)->GetDouble() << ": " << f2D[row] << ": " << f2F[row] << endl;
+			};
+			for (int row = 0; row<400; row++) {
+
+				f3D.push_back(f3Sheet->Cell(row, 1)->GetDouble());
+				f3F.push_back(f3D[row] /*/ 10000*/);
+				std::cout << row << ": " << f1Sheet->Cell(row, 1)->GetDouble() << ": " << f3D[row] << ": " << f3F[row] << endl;
+			};
+			for (int row = 0; row<400; row++) {
+
+				f4D.push_back(f4Sheet->Cell(row, 1)->GetDouble());
+				f4F.push_back(f4D[row] /*/ 10000*/);
+				std::cout << row << ": " << f4Sheet->Cell(row, 1)->GetDouble() << ": " << f4D[row] << ": " << f4F[row] << endl;
+			};
+		}
+		catch (int e) {
+
+			std::cout << "errore Nr. " << e << endl;
+
+		};
+	};
+
+	int i;
+	std::cin >> i;
 
     return 0;
 }
